c6a239ef6c7364d9994a11ef57741ef50c145944
Hygor Luciano <hygorluciano0@gmail.com>
2023-08-02 16:33:53 -0300

    colocando produto

--- src/main/java/org/example/Produto.java
+++ src/main/java/org/example/Produto.java
@@ -1,2 +1,50 @@
-package org.example;public class Produto {
+package org.example;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String nome;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    private String descricao;
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    private BigDecimal preco;
 }
