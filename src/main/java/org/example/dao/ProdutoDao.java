73920ec0240b80e5a225667a2a43d99542b72762
Hygor Luciano <hygorluciano0@gmail.com>
2023-08-03 12:56:09 -0300

    create Dao

---
+++ src/main/java/org/example/dao/ProdutoDao.java
@@ -0,0 +1,17 @@
+package org.example.dao;
+
+import org.example.modelo.Produto;
+
+import javax.persistence.EntityManager;
+
+public class ProdutoDao {
+    private EntityManager em;
+
+    public ProdutoDao(EntityManager em) {
+        this.em = em;
+    }
+    
+    public void cadastrar(Produto produto){
+        this.em.persist(produto);
+    }
+}
--- src/main/java/org/example/teste/CadastraDeProduto.java
+++ src/main/java/org/example/teste/CadastraDeProduto.java
@@ -1,21 +1,20 @@
 package org.example.teste;
 
-import org.example.Produto;
+import org.example.modelo.Produto;
+import org.example.dao.ProdutoDao;
+import org.example.util.JpaUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class CadastraDeProduto {
     public static void main(String[] args) {
         var celuar = new Produto("xiami Redmun", "Muito legal", 800);
-
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-
-        EntityManager em = factory.createEntityManager();
+        
+        EntityManager em = JpaUtil.getEntityManager();
+        var dao = new ProdutoDao(em);
         
         em.getTransaction().begin();
-        em.persist(celuar);
+        dao.cadastrar(celuar);
         em.getTransaction().commit();
         em.close();
     }
