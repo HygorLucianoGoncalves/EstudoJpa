22853d33ef8ba9854fa540c4d26f18ef4855edda
Hygor Luciano <hygorluciano0@gmail.com>
2023-08-03 15:32:10 -0300

    att

--- src/main/java/org/example/teste/CadastraDeProduto.java
+++ src/main/java/org/example/teste/CadastraDeProduto.java
@@ -7,25 +7,37 @@ import org.example.dao.ProdutoDao;
 import org.example.util.JpaUtil;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class CadastraDeProduto {
     public static void main(String[] args) {
-        
-        
-        var celuar = new Produto("xiami Redmun", "Muito legal", 800);
-
-        var celulares = new Categoria("CELULARES");
-        var celularIphone = new Produto("iphone", "Muito Bom", 800,celulares);
-        
+        cadastrarProduto();
         EntityManager em = JpaUtil.getEntityManager();
-        var produtoDao = new ProdutoDao(em);
-        var categoriaDao = new CategoriaDao(em);
-        
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.buscarPorId(1l);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+        todos.forEach(p2 -> System.out.println(p.getNome()));
+
+        Integer precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preco do Produto: " +precoDoProduto);
+    }
+
+    private static void cadastrarProduto() {
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", 800, celulares );
+
+        EntityManager em = JpaUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
         em.getTransaction().begin();
-        
+
         categoriaDao.cadastrar(celulares);
-        produtoDao.cadastrar(celuar);
-        
+        produtoDao.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
     }
